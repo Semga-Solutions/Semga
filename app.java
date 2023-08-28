dbc60ab6975ea35914236f50bc585e0016bfd89c
ChiefSemga <n.semgaalex@yahoo.co.uk>
2023-08-28 09:52:41 +0200

    done

--- app.java
+++ app.java
@@ -1,4 +1,8 @@
 java application 
 development 
 for paypal webapp.
+<<<<<<< HEAD
 Staging
+=======
+ready for deployment in production
+>>>>>>> d3d35ef (app apllication ready for productioin deployment)
--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,4 @@ deployment is ready
 in uat
 QA testing completed
 performance testing is good
+deployment is ready in uat
